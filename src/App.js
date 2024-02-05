7f529a45cd4a904d96fd061a9badc48cb6e41cb2
Abhigyan <abhi2018aray@gmail.com>
2024-02-05 23:01:58 +0530

    running text for one sentence

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-body">
+      <p className="my-intro">Hello I'm </p>
+      <div className="app-main-page">
+        <p className="my-name">Abhigyan Arya.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
