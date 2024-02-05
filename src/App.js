e3a5464bb145994d7599e56a328aa887de1eb3a9
Abhigyan <abhi2018aray@gmail.com>
2024-02-06 00:50:25 +0530

    used some react for two running sentences

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const infoArray = ["Hello I'm Abhigyan Arya", "I'm a full Stack developer"];
+  const [infoArrayIndex, setInfoArrayIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setInfoArrayIndex((prevIndex) => (prevIndex + 1) % infoArray.length);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="main-body">
-      <p className="my-intro">Hello I'm </p>
       <div className="app-main-page">
-        <p className="my-name">Abhigyan Arya.</p>
+        <p className="my-name">{infoArray[infoArrayIndex]}.</p>
       </div>
     </div>
   );
